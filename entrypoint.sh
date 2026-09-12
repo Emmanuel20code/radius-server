c312d64172e934709560d672fe8d470603da5f02
Emmanuel20code <emmanueloyaro3@gmail.com>
2026-09-12 23:04:02 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ echo "DEBUG: Environment variables:"
 env
 
 # Ensure required environment variable is set
-if [ -z "$TAILSCALE_AUTH_KEY" ]; then
+if [ -z "tskey-auth-kn387nZ38911CNTRL-wqa8JBCPX57oThAJaaT157XqLZatuYDDa" ]; then
     echo "ERROR: TAILSCALE_AUTH_KEY must be set."
     exit 1
 fi
@@ -18,7 +18,7 @@ echo "Waiting for tailscaled to start..."
 sleep 5
 
 echo "Authenticating Tailscale..."
-tailscale up --authkey="$TAILSCALE_AUTH_KEY"
+tailscale up --authkey="tskey-auth-kn387nZ38911CNTRL-wqa8JBCPX57oThAJaaT157XqLZatuYDDa"
 
 echo "Tailscale connected. Starting FreeRADIUS..."
 # Check for FreeRADIUS in the correct location dynamically
